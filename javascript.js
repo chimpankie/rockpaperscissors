ba985dd1cf995ac2c51d57e3380803e9577d6375
chimpankie <140263951+chimpankie@users.noreply.github.com>
2023-09-12 19:18:55 +0100

    Create function for one round of the game

--- javascript.js
+++ javascript.js
@@ -2,6 +2,34 @@ function getComputerChoice(){
     let computerChoice = ""
     const choices = ["rock", "paper", "scissors"];
     computerChoice = choices[Math.floor(Math.random()*3)];
-    console.log(computerChoice);
     return computerChoice
+}
+
+function gameRound(playerChoice, computerChoice){
+    let playerHand = playerChoice.toLowerCase();
+    let computerHand = computerChoice.toLowerCase(); 
+    if (playerHand === computerHand){
+        return "draw";
+    } else if (playerHand === "rock"){
+        if (computerHand === "scissors"){
+            return true;
+        }
+        if (computerHand === "paper"){
+            return false;
+        }
+    } else if (playerHand ==="paper"){
+        if (computerHand === "rock"){
+            return true;
+        }
+        if (computerHand === "scissors"){
+            return false;
+        }
+    } else if (playerHand === "scissors"){
+        if (computerHand === "rock"){
+            return false;
+        }
+        if (computerHand === "paper"){
+            return true;
+        }
+    }
 }
